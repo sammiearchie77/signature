fed6e8f7a5c417b7094c5f41fb16013b2acee57a
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-09-28 18:14:16 +0100

    Added Socket.io

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const config = require('./config/env');
+const http = require('http');
 const app = require('./express');
+const { Server } = require('socket.io');
 const mongoose = require('mongoose');
-
+const server = http.createServer(app);
 
 mongoose.Promise = global.Promise
 mongoose.set('strictQuery', false);
@@ -13,11 +15,26 @@ mongoose.connection.on('error', () => {
     throw new Error(`unable to connect to database: ${config.development.database_url}`)
 })
 
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  });
+  
+  io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+  
+    // We can write our socket event listeners in here...
+  });
+
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello Server...'})
 })
 
-app.listen(config.development.port, (err) => {
+
+server.listen(config.development.port, (err) => {
     if (err) {
         console.log(err)
     }
